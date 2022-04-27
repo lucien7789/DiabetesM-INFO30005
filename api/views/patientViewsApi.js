ce6f9b8c06a4fe78e3c7193c1933ffeaeeee2724
Ted Barrett <ted.barrett@student.unimelb.edu.au>
2022-04-27 19:10:30 +1000

    began on profile page

--- api/views/patientViewsApi.js
+++ api/views/patientViewsApi.js
@@ -6,4 +6,8 @@ routes.get("/home", (req, res) => {
     res.render("patient/patientHome.hbs", { title: "Home - Patient"});
 })
 
+routes.get("/profile", (req, res) => {
+    res.render("patient/patientProfile.hbs", { title: "Profile - Patient"});
+})
+
 module.exports = routes;
