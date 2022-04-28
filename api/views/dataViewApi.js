cbe6bc423fa85851a4b09c0a2271e0111e84b488
Ted Barrett <ted.barrett@student.unimelb.edu.au>
2022-04-28 22:28:44 +1000

    visual design of data entry with blood glucose

--- api/views/dataViewApi.js
+++ api/views/dataViewApi.js
@@ -3,7 +3,7 @@ const express = require("express");
 const routes = express.Router();
 
 routes.get("/bloodglucose", (req, res) => {
-    res.render("patient/data-entry/bloodGlucose.hbs", { title: "Recording Health Data"});
+    res.render("patient/data-entry/dataEntry.hbs", {title: "Recording Health Data"});
 });
 
 module.exports = routes;
