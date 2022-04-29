e7468f68c54b3bed4bed4b2cb7cb9a81c3677c27
lucien7789 <68990764+lucien7789@users.noreply.github.com>
2022-04-29 22:49:49 +1000

    Added trust proxy config

--- app.js
+++ app.js
@@ -46,13 +46,20 @@ const mongooseClient = mongoose.connect(uri).then(m => m.connection.getClient())
  * Handle Bars Config Setup
  * ===========================================================
  */
+
+
 app.engine("hbs", exphbs.engine({
     defaultLayout: "about/main",
     layoutsDir: config.projectDir + "/views",
     partialsDir: config.projectDir + "/views",
     extname: "hbs"
 }));
-
+/**
+ * ===========================================================
+ * Express Session Config Setup
+ * ===========================================================
+ */
+app.set('trust proxy', 1);
 app.use(
     expressSession({
         secret: process.env.SESSION_SECRET || "INFO30005",
@@ -62,6 +69,7 @@ app.use(
         cookie: {
             sameSite: "none",
             httpOnly: true,
+            secure: true,
             secure: app.get("env") === "production"
         },
         store: MongoStore.create({ clientPromise: mongooseClient})
